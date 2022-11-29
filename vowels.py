830da8fae452622a86e5a008f988c4ca09475d2a
akshithakartik <112664522+akshithakartik@users.noreply.github.com>
2022-11-29 16:18:17 -0500

    Update vowels.py

--- vowels.py
+++ vowels.py
@@ -2,6 +2,6 @@ sentence = input("Enter the sentence: ")    # the sentence entered by the user
 sentence = sentence.casefold()  # converts the sentence to lowercase for comparison
 count = {'a': 0, 'e': 0, 'i': 0, 'o': 0, 'u': 0}    # create a dictionary with the vowels as keys and initial values as 0
 for char in sentence:   # iterates through each character in the sentence using for loop
-    if char in count:   # checks if the character in the sentence matches any vowel in count
+    if char in count:   # checks if the character in the sentence matches any vowel in the dictionary
             count[char] += 1   # if so, increases the value of the corresponding vowel in the dictionary by 1
 print(count) # displays the final dictionary
